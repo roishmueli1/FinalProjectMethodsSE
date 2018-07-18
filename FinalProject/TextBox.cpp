1b212f5230804f12754e48da6d8bea297cdb2700
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-07-18 12:34:22 +0300

    adding functions

--- FinalProject/TextBox.cpp
+++ FinalProject/TextBox.cpp
@@ -4,10 +4,13 @@
 
 TextBox::TextBox(int width, int height)
 {
-	//this.width = width;
-	//this.height = height;
+	this->width = width;
+	this->height = height;
 }
 
+void TextBox::draw(Graphics& g, int x, int y, size_t z) {
+	
+}
 
 TextBox::~TextBox()
 {
