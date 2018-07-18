3f31402c1aef3df0b10ad3901eb81071464438a6
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-07-18 11:48:55 +0300

    added functions

--- FinalProject/TextBox.h
+++ FinalProject/TextBox.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <string>
-class TextBox
+#include "../Common/Control.h"
+
+class TextBox : public Control
 {
 public:
 	TextBox(int width, int height);
 
+
+	virtual void draw(Graphics& g, int x, int y, size_t z) {};
 	//Setters
 	//void setText(string);
 
