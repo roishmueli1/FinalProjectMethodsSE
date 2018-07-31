edc4aadb6c6bae9316782fb2e45adb5a22c22809
avimi <avi.michaeli.1992@gmail.com>
2018-07-31 19:25:18 +0300

    Add files via upload

---
+++ NumericBox.cpp
@@ -0,0 +1,15 @@
+#include "NumericBox.h"
+
+
+
+NumericBox::NumericBox(int x, int y, int width, int height,int number)
+{
+	Graphics g;
+	g.setBackground(Color::Black);
+	g.setForeground(Color::Green);
+	age.push_back("22");
+	age.push_back("26");
+	age.push_back("32");
+
+
+}
---
+++ NumericBox.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include "../Common/EventEngine.h"
+#include "../Common/Graphics.h"
+
+
+class NumericBox : public Control
+{
+
+
+private:
+
+	int a, b, c;
+	vector<string>age;
+
+
+
+public:
+
+	NumericBox(int left,int bottom,int width,int height, int number);
+
+
+
+
+
+};
